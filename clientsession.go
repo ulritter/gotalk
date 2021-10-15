8d9738840d7c3842bf1366f55a0ea3c891f71e2e
Uwe Ritter <ulritter@gmail.com>
2021-10-15 12:44:05 +0200

    client side graceful signal and quit handling

--- clientsession.go
+++ clientsession.go
@@ -144,6 +144,7 @@ func (a *application) handleClientSession(connect string, config *tls.Config, ni
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
 	go func() {
+		//intercept signal and start closing roundtrip
 		<-c
 		sendMessage(conn, ACTION_EXIT, []string{""})
 	}()
